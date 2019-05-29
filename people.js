28f6731bfd5a1e22491e9db7b479e658fcaed3fc
danielamuniz367 <danielamuniz367@gmail.com>
2019-05-28 20:55:15 -0500

    updated query for getPeople

--- people.js
+++ people.js
@@ -14,7 +14,7 @@ module.exports = function(){
     }
 
     function getPeople(res, mysql, context, complete){
-        mysql.pool.query("SELECT id, fname, lname, houses.name AS house_id, class_year FROM students INNER JOIN houses ON house_id = houses.id", function(error, results, fields){
+        mysql.pool.query("SELECT id, fname, lname, house_id, class_year FROM students INNER JOIN houses ON house_id = houses.id", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
